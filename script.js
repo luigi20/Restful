a4e92830a5f5594214ac87b09ee8c94e1d923499
Luis <luisopentec@gmail.com>
2021-10-07 15:38:17 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const app = express();
+const data = require('./data.json');
+
+app.use(express.json());
+
+app.get('/clients', function (req, res) {
+    res.json(data);
+});
+
+app.get('/clients:id', function (req, res) {
+
+});
+
+app.post('/clients', function (req, res) {
+
+});
+
+app.put('/clients:id', function (req, res) {
+
+})
+
+app.delete('/clients:id', function (req, res) {
+
+});
+
+app.listen(3000, function () {
+    console.log('Server listening on port 3000');
+});
